22693c9f1ad5debe5681cd4d7026b599c1407de0
Axel <axel@reinemuth.de>
2024-05-16 19:14:32 +0200

    Initial commit

---
+++ MSFSVariableWatcher/MSFSService.cs
@@ -0,0 +1,23 @@
+﻿using FSUIPC;
+
+namespace MSFSVariableWatcher
+{
+    public static class MSFSService
+    {
+        public static void InitMSFSServices()
+        {
+            // Handle events
+            MSFSVariableServices.OnLogEntryReceived += VS_OnLogEntryReceived; // Fired when the WASM module sends a log entry
+
+            MSFSVariableServices.Init(); // Initialise 
+            MSFSVariableServices.LogLevel = LOGLEVEL.LOG_LEVEL_INFO; // Set the level of logging
+        }
+
+        private static void VS_OnLogEntryReceived(object? sender, LogEventArgs e)
+        {
+            Console.WriteLine(e.LogEntry);
+        }
+
+        public static void Start() => MSFSVariableServices.Start();
+    }
+}
---
+++ MSFSVariableWatcher/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MSFSVariableWatcher.Data;
+using FSUIPC;
+
+namespace MSFSVariableWatcher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MSFSService.InitMSFSServices();
+            MSFSService.Start();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+
+            builder.WebHost.UseUrls("http://localhost:7672");
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
